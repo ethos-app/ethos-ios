efd620f687cce0dbace5d16cf146001f5fb2e977
iscottonline <Scott@scotts-mbp.mobile.asu.edu>
2016-08-31 11:23:14 -0700

    first commit

---
+++ Ethos/BizCardTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  BizCardTableViewCell.swift
+//  Ethos
+//
+//  Created by Scott Fitsimones on 8/30/16.
+//  Copyright © 2016 Bolt Visual, Inc. All rights reserved.
+//
+
+import UIKit
+
+class BizCardTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
